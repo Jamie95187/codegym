4e531c2a20c6d7b67593efbe6f2636657af418ff
Jamie <jamiewong63@googlemail.com>
2020-11-26 22:20:46 +0000

    string interpolation task

---
+++ 1.JavaSyntax/src/com/codegym/task/task01/task0103/Solution.java
@@ -0,0 +1,13 @@
+package com.codegym.task.task01.task0103;
+
+/* 
+My young friend
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 3126;
+        System.out.println(Integer.toString(i-8));
+    }
+}
