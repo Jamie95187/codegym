f0a4d906ed793dae579e74e9e1322137bc0b021d
Jamie <jamiewong63@googlemail.com>
2020-11-29 09:25:57 +0000

    add game object class

---
+++ 8.Games/src/com/codegym/games/minesweeper/GameObject.java
@@ -0,0 +1,13 @@
+package com.codegym.games.minesweeper;
+
+public class GameObject {
+
+    public int x;
+    public int y;
+
+    public GameObject (int xCor, int yCor) {
+        x = xCor;
+        y = yCor;
+    }
+
+}
