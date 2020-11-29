72c080cbe5ec9927e9c11548bb9c6320f545e898
Jamie <jamiewong63@googlemail.com>
2020-11-29 13:24:40 +0000

    initializing minesweeper grid

--- 8.Games/src/com/codegym/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/codegym/games/minesweeper/MinesweeperGame.java
@@ -5,8 +5,20 @@ public class MinesweeperGame extends Game {
 
     private static final int SIDE = 9;
 
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+
     public void initialize(){
         setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame() {
+        for(int x = 0; x < SIDE; x++) {
+            for(int y = 0; y < SIDE; y++) {
+                gameField[y][x] = new GameObject(x, y);
+                setCellColor(x, y, Color.ORANGE);
+            }
+        }
     }
 
 }
