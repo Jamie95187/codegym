2c0e925c21dd5f3fc1b723c4a31d1cb3d6a95218
Jamie <jamiewong63@googlemail.com>
2020-11-30 05:57:48 +0000

    randomly generate status of current cell

--- 8.Games/src/com/codegym/games/minesweeper/GameObject.java
+++ 8.Games/src/com/codegym/games/minesweeper/GameObject.java
@@ -4,10 +4,12 @@ public class GameObject {
 
     public int x;
     public int y;
+    public boolean isMine;
 
-    public GameObject (int xCor, int yCor) {
+    public GameObject (int xCor, int yCor, boolean mineStatus) {
         x = xCor;
         y = yCor;
+        isMine = mineStatus;
     }
 
 }
--- 8.Games/src/com/codegym/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/codegym/games/minesweeper/MinesweeperGame.java
@@ -4,6 +4,7 @@ import com.codegym.engine.cell.*;
 public class MinesweeperGame extends Game {
 
     private static final int SIDE = 9;
+    private int countMinesOnField = 0;
 
     private GameObject[][] gameField = new GameObject[SIDE][SIDE];
 
@@ -15,7 +16,12 @@ public class MinesweeperGame extends Game {
     private void createGame() {
         for(int x = 0; x < SIDE; x++) {
             for(int y = 0; y < SIDE; y++) {
-                gameField[y][x] = new GameObject(x, y);
+                if (getRandomNumber(10) == 1) {
+                    gameField[y][x] = new GameObject(x, y, true);
+                    countMinesOnField++;
+                } else {
+                    gameField[y][x] = new GameObject(x, y, false);
+                }
                 setCellColor(x, y, Color.ORANGE);
             }
         }
