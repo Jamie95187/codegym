0e7732bff3f11eb2ea63750cada80c5072181300
Jamie <jamiewong63@googlemail.com>
2020-12-06 13:38:03 +0000

    Snake Game: task three create gameobject class

---
+++ 8.Games/src/com/codegym/games/snake/GameObject.java
@@ -0,0 +1,12 @@
+package com.codegym.games.snake;
+
+public class GameObject {
+
+    public int x;
+    public int y;
+
+    public GameObject(int xCor, int yCor){
+        x = xCor;
+        y = yCor;
+    }
+}
